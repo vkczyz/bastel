a6a289628df6542f99f572b9310066c0570f67b2
mzyzc <matthew@czyz.xyz>
2023-02-10 16:30:22 +0000

    Implemented XML parsing for AudioComponent

--- src/components/audio.rs
+++ src/components/audio.rs
@@ -1,4 +1,5 @@
 use crate::components::Component;
+use roxmltree;
 
 #[allow(dead_code)]
 pub struct AudioComponent {
@@ -13,6 +14,33 @@ impl AudioComponent {
             AudioComponent {
                 muted: false,
                 bgm: None,
+                sfx: None,
+            }
+        )
+    }
+
+    pub fn from_xml(data: roxmltree::Node) -> Component {
+        let mut muted = false;
+        let mut bgm = None;
+
+        data.children()
+            .filter(|n| n.is_element())
+            .map(|n| {
+                match n.tag_name().name() {
+                    "muted" => muted = true,
+                    "bgm" => match n.attribute("bgm") {
+                        Some(b) => bgm = Some(String::from(b)),
+                        _ => (),
+                    },
+                    _ => (),
+                }
+            }
+        ).for_each(drop);
+
+        Component::Audio(
+            AudioComponent {
+                muted,
+                bgm,
                 sfx: None,
             }
         )
